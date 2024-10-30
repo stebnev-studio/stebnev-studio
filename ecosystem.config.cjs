c243d49b95ebaaf654abc0950b49bb481639f1c9
Stebnev Support <support@stebnev-studio.ru>
2024-10-31 02:29:52 +0300

    has changed server

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,16 @@
 module.exports = {
-    apps: [
-      {
-        name: 'Stebnev-Studio',
-        port: 3000,
-        exec_mode: 'cluster',
-        instances: 'max',
-        script: './server/index.mjs'
-      }
-    ],
-  }
+  apps: [
+    {
+      name: "Stebnev-Studio",
+      port: 3000,
+      exec_mode: "cluster",
+      instances: "max", // Использует максимальное количество ядер
+      script: "./.output/server/index.mjs", // Путь к вашему серверному файлу Nuxt
+      env: {
+        NODE_ENV: "production",
+        HOST: "127.0.0.1",
+        PORT: 3000,
+      },
+    },
+  ],
+};
